059a41711c1604d04814e725ad8fc12d211c5ea3
Liam Sho <conan_creeper@outlook.com>
2021-12-14 16:48:18 +0800

    feat.添加 Host

---
+++ Konverter/Hosting.cs
@@ -0,0 +1,59 @@
+﻿// Konverter - Plain text file style converter
+// Copyright (C) 2021 Witches Banquet
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License,
+// or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+using Microsoft.Extensions.Hosting;
+
+namespace Konverter;
+
+public class Hosting : IHostedService
+{
+    private readonly IHostApplicationLifetime _lifetime;
+    private readonly ILogger<Hosting> _logger;
+
+    public Hosting(IHostApplicationLifetime lifetime, ILogger<Hosting> logger)
+    {
+        _lifetime = lifetime;
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hosting start called");
+        _logger.LogInformation("Hosting starting");
+
+        _lifetime.ApplicationStarted.Register(OnStarted);
+        _lifetime.ApplicationStopping.Register(OnStopping);
+        _lifetime.ApplicationStopped.Register(OnStopped);
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hosting stop called");
+        return Task.CompletedTask;
+    }
+
+    private void OnStarted()
+    {
+        _logger.LogInformation("Hosting started");
+    }
+
+    private void OnStopping()
+    {
+        _logger.LogInformation("Hosting stopping");
+    }
+
+    private void OnStopped()
+    {
+        _logger.LogInformation("Hosting stopped");
+    }
+}
--- Konverter/Program.cs
+++ Konverter/Program.cs
@@ -9,4 +9,53 @@
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY
 
-Console.WriteLine("Hello World!");
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+#region Setup Logging Path
+
+var dllPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath);
+if (dllPath is null)
+{
+    AnsiConsole.WriteException(new ArgumentNullException(nameof(dllPath), "Executing assembly path is null"));
+    return;
+}
+var loggingFile = Path.Combine(dllPath, "Logs", $"log-{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.log");
+
+#endregion
+
+#region Setup Serilog
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(loggingFile)
+    .CreateLogger();
+
+#endregion
+
+#region Host Builder
+
+var builder = Host.CreateDefaultBuilder();
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddHostedService<Hosting>();
+});
+
+builder.ConfigureLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+});
+
+builder.UseSerilog();
+
+#endregion
+
+#region Run
+
+var app = builder.Build();
+
+await app.RunAsync();
+
+#endregion
